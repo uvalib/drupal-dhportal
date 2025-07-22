136505c7e99882016ae585cd453d94175ecd3304
ys2n <ys2n@virginia.edu>
2025-07-21 21:02:03 -0400

    Add admin auth source to SimpleSAMLphp configuration
    - Add required 'admin' auth source using core:AdminPassword
    - This resolves the 'Cannot find admin auth source' error
    - Admin auth source is required for SimpleSAMLphp administration interface

--- package/data/files/opt/drupal/simplesamlphp/config/authsources.php
+++ package/data/files/opt/drupal/simplesamlphp/config/authsources.php
@@ -5,6 +5,11 @@
  */
 
 $config = [
+    // Admin authentication source - required for SimpleSAMLphp administration
+    'admin' => [
+        'core:AdminPassword',
+    ],
+
     // Default SP configuration - connects to production IdP
     'default-sp' => [
         'saml:SP',
